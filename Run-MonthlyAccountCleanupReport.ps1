0a8355d70ab5f1065e95f59571897714506a0703
bassetlife <ruben@rubenreyes.net>
2018-12-04 12:43:47 -0600

    Update Run-MonthlyAccountCleanupReport.ps1
    Modified code/syntax

--- Run-MonthlyAccountCleanupReport.ps1
+++ Run-MonthlyAccountCleanupReport.ps1
@@ -1,5 +1,4 @@
-﻿function Run-MonthlyAccountCleanupReport{
-Import-Module ActiveDirectory
+﻿Import-Module ActiveDirectory
 
 $Stage3OU = "OU=Stage3,OU=Staged-less90Days,OU=Disabled Accounts,OU=US,DC=ezcorp,DC=com"
 
@@ -8,13 +7,12 @@ $anonPassword = ConvertTo-SecureString -String "anonymous" -AsPlainText -Force
 $anonCredentials = New-Object System.Management.Automation.PSCredential($anonUsername, $anonPassword)
 
 $Stage3Accounts = Get-ADUser -properties * -SearchBase $Stage3OU -Filter { (Enabled -eq $false) } |
-Select-Object @{Label = "First Name";Expresion = {$_.GivenName}}, 
-              @{Label = "Last Name";Expresion = {$_.Surname}},
-              @{Label = "Username";Expresion = {$_.SamAccountname}},
-              @{Label = "Description";Expresion = {$_.Description}},
-              @{Label = "Employee ID";Expresion = {$_.EmployeeID}} | 
-ConvertTo-Html
-Send-MailMessage -To "ruben_reyes@ezcorp.com" -From "techsupport@ezcorp.com" -Subject "Monthly Account Cleanup Notice" 
--Body "The following accounts will be processed for deletion in accordance with our account management process. Please contact 
+Select-Object @{Label="First Name";Expression = {$_.GivenName}}, 
+              @{Label="Last Name";Expression = {$_.Surname}},
+              @{Label="Username";Expression = {$_.SamAccountname}},
+              @{Label="Description";Expression = {$_.Description}},
+              @{Label="Employee ID";Expression = {$_.EmployeeID}} | ConvertTo-Html
+
+Send-MailMessage -To "ruben_reyes@ezcorp.com" -From "techsupport@ezcorp.com" -Subject "Monthly Account Cleanup Notice" -Body "The following accounts will be processed for deletion in accordance with our account management process. Please contact 
 WinEngineeringTeam@ezcorp.com should you have any questions or concerns. $Stage3Accounts" -BodyAsHtml -SmtpServer "ezmail-out.ezcorp.com" -Port 25 -Credential $anonCredentials
-}
+
