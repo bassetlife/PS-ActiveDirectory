892cc1bb985d2dfc8befb3225be589966385ccaf
bassetlife <ruben@rubenreyes.net>
2018-12-03 19:06:15 -0600

    Created Labels

--- Run-MonthlyAccountCleanupReport.ps1
+++ Run-MonthlyAccountCleanupReport.ps1
@@ -8,7 +8,11 @@ $anonPassword = ConvertTo-SecureString -String "anonymous" -AsPlainText -Force
 $anonCredentials = New-Object System.Management.Automation.PSCredential($anonUsername, $anonPassword)
 
 $Stage3Accounts = Get-ADUser -properties * -SearchBase $Stage3OU -Filter { (Enabled -eq $false) } |
-Select-Object GivenName, Surname, SamAccountname, Description, EmployeeID | 
+Select-Object @{Label = "First Name";Expresion = {$_.GivenName}}, 
+              @{Label = "Last Name";Expresion = {$_.Surname}},
+              @{Label = "Username";Expresion = {$_.SamAccountname}},
+              @{Label = "Description";Expresion = {$_.Description}},
+              @{Label = "Employee ID";Expresion = {$_.EmployeeID}} | 
 ConvertTo-Html
 Send-MailMessage -To "ruben_reyes@ezcorp.com" -From "techsupport@ezcorp.com" -Subject "Monthly Account Cleanup Notice" 
 -Body "The following accounts will be processed for deletion in accordance with our account management process. Please contact 
