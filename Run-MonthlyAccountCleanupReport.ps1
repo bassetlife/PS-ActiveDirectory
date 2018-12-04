64a8a5fd6a1fc1931b5e34b04c2b6bba08189dfb
bassetlife <ruben@rubenreyes.net>
2018-12-03 18:10:13 -0600

    Update Run-MonthlyAccountCleanupReport.ps1

--- Run-MonthlyAccountCleanupReport.ps1
+++ Run-MonthlyAccountCleanupReport.ps1
@@ -10,7 +10,7 @@ $anonCredentials = New-Object System.Management.Automation.PSCredential($anonUse
 $Stage3Accounts = Get-ADUser -properties * -SearchBase $Stage3OU -Filter { (Enabled -eq $false) } |
 Select-Object GivenName, Surname, SamAccountname, Description, EmployeeID | 
 ConvertTo-Html
-Send-MailMessage -To "ruben_reyes@ezcorp.com" -From "monthlycleanupnotice@ezcorp.com" -Subject "Monthly Account Cleanup Notice" 
+Send-MailMessage -To "ruben_reyes@ezcorp.com" -From "techsupport@ezcorp.com" -Subject "Monthly Account Cleanup Notice" 
 -Body "The following accounts will be processed for deletion in accordance with our account management process. Please contact 
 WinEngineeringTeam@ezcorp.com should you have any questions or concerns. $Stage3Accounts" -BodyAsHtml -SmtpServer "ezmail-out.ezcorp.com" -Port 25 -Credential $anonCredentials
 }
