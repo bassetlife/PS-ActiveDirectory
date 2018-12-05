1533fba221054647c5b64dfbfb368612288f4f47
bassetlife <ruben@rubenreyes.net>
2018-12-05 09:25:58 -0600

    Update AccountCleanupReport.ps1

--- AccountCleanupReport.ps1
+++ AccountCleanupReport.ps1
@@ -13,6 +13,6 @@ Select-Object @{Label="First Name";Expression = {$_.GivenName}},
               @{Label="Description";Expression = {$_.Description}},
               @{Label="Employee ID";Expression = {$_.EmployeeID}} | ConvertTo-Html
 
-Send-MailMessage -To "ruben_reyes@ezcorp.com" -From "techsupport@ezcorp.com" -Subject "Monthly Account Cleanup Notice" -Body "The following accounts will be processed for deletion in accordance with our account management process. Please contact 
+Send-MailMessage -To "monthlycleanupnotice@ezcorp.com" -From "techsupport@ezcorp.com" -Subject "Monthly Account Cleanup Notice" -Body "The following accounts will be processed for deletion in accordance with our account management process. Please contact 
 WinEngineeringTeam@ezcorp.com should you have any questions or concerns. $Stage3Accounts" -BodyAsHtml -SmtpServer "ezmail-out.ezcorp.com" -Port 25 -Credential $anonCredentials
 
