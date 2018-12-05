c3ffb217c00134131837be570dc02f3f1cb4b14c
bassetlife <ruben@rubenreyes.net>
2018-12-05 09:21:22 -0600

    cleanup

--- Run-MonthlyAccountCleanupReport.ps1
+++ Run-MonthlyAccountCleanupReport.ps1
@@ -1,5 +1,4 @@
-﻿function Run-MonthlyAccountCleanupReport{
-Import-Module ActiveDirectory
+﻿Import-Module ActiveDirectory
 
 $Stage3OU = "OU=Stage3,OU=Staged-less90Days,OU=Disabled Accounts,OU=US,DC=ezcorp,DC=com"
 
@@ -10,7 +9,7 @@ $anonCredentials = New-Object System.Management.Automation.PSCredential($anonUse
 
 $Stage3Accounts = Get-ADUser -properties * -SearchBase $Stage3OU -Filter { (Enabled -eq $false) } | select GivenName, Surname, SamAccountname, Description, EmployeeID | ConvertTo-Html
 Send-MailMessage -To "ruben_reyes@ezcorp.com" -From "monthlycleanupnotice@ezcorp.com" -Subject "Monthly Account Cleanup Notice" -Body "The following accounts will be processed for deletion in accordance with our account management process. Please contact WinEngineeringTeam@ezcorp.com should you have any questions or concerns. $Stage3Accounts" -BodyAsHtml -SmtpServer "ezmail-out.ezcorp.com" -Port 25 -Credential $anonCredentials
-=======
+
 $Stage3Accounts = Get-ADUser -properties * -SearchBase $Stage3OU -Filter { (Enabled -eq $false) } |
 Select-Object @{Label = "First Name";Expresion = {$_.GivenName}}, 
               @{Label = "Last Name";Expresion = {$_.Surname}},
@@ -21,4 +20,4 @@ ConvertTo-Html
 Send-MailMessage -To "ruben_reyes@ezcorp.com" -From "techsupport@ezcorp.com" -Subject "Monthly Account Cleanup Notice" 
 -Body "The following accounts will be processed for deletion in accordance with our account management process. Please contact 
 WinEngineeringTeam@ezcorp.com should you have any questions or concerns. $Stage3Accounts" -BodyAsHtml -SmtpServer "ezmail-out.ezcorp.com" -Port 25 -Credential $anonCredentials
-    }
+
