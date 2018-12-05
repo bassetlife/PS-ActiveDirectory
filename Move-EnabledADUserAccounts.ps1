5805d48ee3a6684b15e72fb497941dcb5fd281b0
bassetlife <ruben@rubenreyes.net>
2018-12-05 08:56:14 -0600

    Update Move-EnabledADUserAccounts.ps1
    Removed function

--- Move-EnabledADUserAccounts.ps1
+++ Move-EnabledADUserAccounts.ps1
@@ -1,4 +1,3 @@
-function Move-EnabledADUserAccounts{
     Import-Module ActiveDirectory
 
         $Stage3OU = "OU=Stage3,OU=Staged-less90Days,OU=Disabled Accounts,OU=US,DC=ezcorp,DC=com"
@@ -7,7 +6,7 @@ function Move-EnabledADUserAccounts{
     $EnabledAccounts = Get-ADUser -properties * -Server 'EZUSDC07' -SearchBase $Stage3OU -Filter { (Enabled -eq $true) }
 
         foreach ($Account in $EnabledAccounts)
-    {
+        {
         Move-ADObject -Identity $Account -TargetPath $StatusUnknownOU
-    }
+        }
 
