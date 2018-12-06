ea4e07f801502a2063020e14f657f333981002e1
bassetlife <ruben@rubenreyes.net>
2018-12-06 14:07:09 -0600

    Create 90DayInactivityReport.ps1

---
+++ 90DayInactivityReport.ps1
@@ -0,0 +1,3 @@
+﻿Import-Module ActiveDirectory
+$TodaysDate = Get-Date -Format ddMMMyyyy
+Search-ADAccount -AccountInactive -TimeSpan 90.00:00:00 | where {$_.ObjectClass -eq 'user' -and $_.Enabled -eq $true} | Select Name,Description,ObjectClass | Export-CSV c:\temp\90InactivityReport$TodaysDate.csv -NoTypeInformation
