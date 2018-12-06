1596260f489d0fa937112b21b373fb85e8c1ff38
bassetlife <ruben@rubenreyes.net>
2018-12-06 14:03:59 -0600

    Create NoEmployeeIDReport.ps1

---
+++ NoEmployeeIDReport.ps1
@@ -0,0 +1,3 @@
+﻿Import-Module ActiveDirectory
+$TodaysDate = Get-Date -Format ddMMMyyyy 
+get-aduser -filter * -Properties * | Where-Object {$_.Enabled -eq $true -and $_.EmployeeID -eq $null} | Select GivenName,Surname,Description,EmployeeID,Enabled,SamAccountName,EmailAddress,CanonicalName | Export-Csv c:\temp\NoEmployeeIDAccounts$TodaysDate.csv -NoTypeInformation
