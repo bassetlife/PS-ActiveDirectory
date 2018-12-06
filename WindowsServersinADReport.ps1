93315eb0d51c04786789ed94f06a83147488fb47
bassetlife <ruben@rubenreyes.net>
2018-12-06 14:09:27 -0600

    Create WindowsServersinADReport.ps1

---
+++ WindowsServersinADReport.ps1
@@ -0,0 +1,3 @@
+﻿Import-Module activedirectory 
+$TodaysDate = Get-Date -Format ddMMMyyyy
+Get-ADComputer -Filter {OperatingSystem -Like 'Windows Server*'} -Property * | Format-Table Name,OperatingSystem,OperatingSystemServicePack -Wrap -Auto | Export-Csv c:\Temp\AllWindowsServers$TodaysDate.csv
